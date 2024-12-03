bf8b6a0fb929bf225a933e649cfba7f208026f4e
Alan Lu <alu36@ucsc.edu>
2024-12-02 20:51:34 -0800

    Animation set up

--- Prism of The Improbable/Assets/Scripts/PlayerMovement.cs
+++ Prism of The Improbable/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
+
     public float runSpeed = 40f;
     float horizontalMove = 0f;
     bool jump = false;
@@ -14,10 +16,18 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("IsJumping", true);
         }
+    }
+
+    public void OnLanding ()
+    {
+        animator.SetBool("IsJumping", false);
     }
 
     void FixedUpdate ()
